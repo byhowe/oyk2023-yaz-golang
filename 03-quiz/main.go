f2485324509afb74f869c3ee05adc09032017fd8
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2023-08-28 16:10:52 +0300

    Add go quiz solutions

---
+++ 03-quiz/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+type Descirabable interface {
+	Description() string
+}
+
+type Item struct {
+	// Name of the product
+	Name string
+	// Price without the discount
+	RawPrice uint
+	// Discount amount
+	Discount uint
+}
+
+// Convert kuruş to lira
+func kurusToLira(kurus uint) float64 {
+	return float64(kurus) / 100.0
+}
+
+// Calculate the discounted price in kuruş.
+func (self Item) calculatePrice() uint {
+	return self.RawPrice - self.Discount
+}
+
+// Calculate the discounted price in liras.
+func (self Item) CalculatePrice() float64 {
+	return float64(self.calculatePrice()) / 100.0
+}
+
+// Return a description string.
+//
+// Includes the name and the price information about the item.
+func (self Item) Description() string {
+	return fmt.Sprintf("%Q", self)
+}
+
+// Custom formatter that prints the description of the item using the 'Q' verb.
+func (self Item) Format(f fmt.State, verb rune) {
+	switch verb {
+	case rune('Q'):
+		if self.Discount != 0 {
+			fmt.Fprintf(
+				f,
+				"%s - %v (%v TL indirimle %v TL)",
+				self.Name,
+				kurusToLira(self.RawPrice),
+				kurusToLira(self.Discount),
+				self.CalculatePrice(),
+			)
+		} else {
+			fmt.Fprintf(f, "%s - %v TL", self.Name, self.CalculatePrice())
+		}
+	default:
+		fmt.Fprint(f, self)
+	}
+}
+
+// Sum total price of items with discount.
+func TotalPrice(items []Item) float64 {
+	var total uint
+	for _, item := range items {
+		total += item.calculatePrice()
+	}
+	return kurusToLira(total)
+}
+
+func main() {
+	items := []Item{
+		{Name: "Elma", RawPrice: 75, Discount: 7},
+		{Name: "Portakal", RawPrice: 100, Discount: 0},
+	}
+
+	for _, item := range items {
+		fmt.Println(item.Description())
+	}
+
+	total := TotalPrice(items)
+	fmt.Printf("Toplam Fiyat: %v TL\n", total)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module oyk
+
+go 1.21.0
